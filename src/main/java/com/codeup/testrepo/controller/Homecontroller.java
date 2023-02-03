468c42189b7b0ddd49d181d1b7203f9fc8205b2f
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-02-03 10:44:52 -0600

    completed test repo

---
+++ src/main/java/com/codeup/testrepo/controller/Homecontroller.java
@@ -0,0 +1,16 @@
+package com.codeup.testrepo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class Homecontroller {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String hello() {
+        return "test repo";
+    }
+}
