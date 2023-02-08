798519754e02fc490845011599a9b14497eaf65a
JordanWitt <wittjordan94@gmail.com>
2023-02-08 17:05:29 -0600

    added api to app of the profiles

---
+++ src/main/resources/templates/js/home-not-logged.js
@@ -0,0 +1,69 @@
+
+// This returns houses that have recently been sold in the houston area
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '4b79262d6emsh7cc3f4048fc7718p12b945jsndb86ef3ab993',
+        'X-RapidAPI-Host': 'zillow-com1.p.rapidapi.com'
+    }
+};
+let imgSrc
+let address
+let propertyType
+let listing
+
+let appendHtml = document.getElementById("results")
+let appendHtmlTwo = document.getElementById("results-two")
+function getListings() {
+    fetch('https://zillow-com1.p.rapidapi.com/propertyExtendedSearch?location=houston%2C%20Tx&page=1&status_type=RecentlySold&home_type=Houses&sort=Homes_for_You', options)
+        .then(response => response.json())
+        .then(data => {
+            imgSrc = data.props[0].imgSrc
+            address = data.props[0].address
+            propertyType = data.props[0].propertyType
+            console.log(imgSrc);
+            console.log(address);
+            console.log(propertyType);
+
+            let html = `
+                        <div class="card mb-3">
+                        <img class="card-img-top" src="${data.props[2].imgSrc}" alt="Card image cap">
+                        <div class="card-body">
+                        <h5 class="card-title">Featured Home</h5>
+                        <p class="card-text">${data.props[2].address}</p>
+                        <p class="card-text"><small class="text-muted">${data.props[2].propertyType}</small></p>
+                        </div>
+                        </div>`
+
+            let html2 = `<div class="card mb-3" id="btm-card">
+                        <img class="card-img-top" src="${data.props[8].imgSrc}" alt="Card image cap">
+                        <div class="card-body">
+                        <h5 class="card-title">Featured Home</h5>
+                        <p class="card-text">${data.props[8].address}</p>
+                        <p class="card-text"><small class="text-muted">${data.props[8].propertyType}</small></p>
+                        </div>
+                        </div>
+
+                        <div class="card mb-3" id="btm-card">
+                        <img class="card-img-top" src="${data.props[3].imgSrc}" alt="Card image cap">
+                        <div class="card-body">
+                        <h5 class="card-title">Featured Home</h5>
+                        <p class="card-text">${data.props[3].address}</p>
+                        <p class="card-text"><small class="text-muted">${data.props[3].propertyType}</small></p>
+                        </div>
+                        </div>
+                           
+                        <div class="card mb-3" id="btm-card">
+                        <img class="card-img-top" src="${data.props[4].imgSrc}" alt="Card image cap">
+                        <div class="card-body">
+                        <h5 class="card-title">Featured Home</h5>
+                        <p class="card-text">${data.props[4].address}</p>
+                        <p class="card-text"><small class="text-muted">${data.props[4].propertyType}</small></p>
+                        </div>
+                        </div>`
+            $(appendHtml).append(html)
+            $(appendHtmlTwo).append(html2)
+
+        })
+}
+getListings();
