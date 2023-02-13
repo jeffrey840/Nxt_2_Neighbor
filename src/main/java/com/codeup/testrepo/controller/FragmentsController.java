a9c4735546f7bf737e72585c04a6a78bf4f7c319
Lucas Slade <lucas.sheldon.slade@gmail.com>
2023-02-13 11:25:34 -0700

    refactored

--- src/main/java/com/codeup/testrepo/controller/FragmentsController.java
+++ src/main/java/com/codeup/testrepo/controller/FragmentsController.java
@@ -1,13 +1,9 @@
 package com.codeup.testrepo.controller;
 
-import ch.qos.logback.core.model.Model;
 import com.codeup.testrepo.models.User;
-import com.codeup.testrepo.models.CreateListing;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class FragmentsController {
