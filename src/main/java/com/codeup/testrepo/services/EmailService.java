89a3bb5c113825c9277b6059ffa696298fe2adde
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-02-14 22:04:52 -0600

    updated properties

--- src/main/java/com/codeup/testrepo/services/EmailService.java
+++ src/main/java/com/codeup/testrepo/services/EmailService.java
@@ -2,7 +2,7 @@ package com.codeup.testrepo.services;
 
 import com.codeup.testrepo.models.Listings;
 
-
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
